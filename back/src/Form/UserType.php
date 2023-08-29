abc80256e0c86f5a8f61bc0a3bc5abe41c9c0f73
Chrstn67 <chrstn.hmbrt67@outlook.com>
2023-08-29 14:02:56 +0200

    en cour de debug

--- back/src/Form/UserType.php
+++ back/src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -28,8 +29,13 @@ class UserType extends AbstractType
                 ],
             ])
             ->add('email')
-            ->add('password');
-        // ->add('reservation');
+            ->add('password')
+            ->add('reservation', CollectionType::class, [
+                'entry_type' => ReservationType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
