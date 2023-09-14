1d61faee584685f73b34a56f4712dce1445e588a
Chrstn67 <chrstn.hmbrt67@outlook.com>
2023-09-14 09:51:24 +0200

    RAS

--- back/tests/Entity/StandTest.php
+++ back/tests/Entity/StandTest.php
@@ -9,14 +9,14 @@ class StandTest extends TestCase
 {
     public function testStandEntity()
     {
-        
+
         $stand = new Stand();
 
-        // Utilisez les méthodes de l'entité pour définir ses propriétés
+
         $stand->setStandName('Nom du stand');
         $stand->setLocation('Emplacement du stand');
 
-        
+
         $this->assertEquals('Nom du stand', $stand->getStandName());
         $this->assertEquals('Emplacement du stand', $stand->getLocation());
     }
