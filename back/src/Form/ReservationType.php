1a6e9365151b9c562ef3913c27f024175be03aa7
Christian <chrst.hmbrt67@outlook.com>
2023-10-31 15:48:56 +0100

    debug reasa plusieurs user en cours

--- back/src/Form/ReservationType.php
+++ back/src/Form/ReservationType.php
@@ -2,19 +2,35 @@
 
 namespace App\Form;
 
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 use App\Entity\Reservation;
 use App\Entity\User;
 use App\Entity\Stand;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ReservationType extends AbstractType
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $users = $this->entityManager->getRepository(User::class)->findAll();
+
+        $choices = [];
+        foreach ($users as $user) {
+            $choices[$user->getName()] = $user->getId();
+        }
+
         $builder
             ->add('calendar_date')
             ->add('hour_time')
@@ -26,13 +42,18 @@ class ReservationType extends AbstractType
                 ],
             ])
             ->add('created_at')
-            ->add('User', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'name', 
+            ->add('User', ChoiceType::class, [
+                'choices' => $choices,
+                'multiple' => true,
+                'expanded' => true,
+                'label' => 'Utilisateurs',
+                'label_attr' => [
+                    'class' => 'checkbox-inline',
+                ],
             ])
             ->add('Stand', EntityType::class, [
                 'class' => Stand::class,
-                'choice_label' => 'stand_name', 
+                'choice_label' => 'stand_name',
             ]);
     }
 
